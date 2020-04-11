52a5dd9227f7e31f9afd185508c012345c35e367
NoahNovick <nnovickfaille1@gmail.com>
2020-04-11 18:17:06 -0400

    store name function

--- extension/browser.js
+++ extension/browser.js
@@ -1,132 +1,129 @@
 let product = {};
 
 //
-	chrome.runtime.onMessage.addListener(function (
-		request,
-		sender,
-		sendResponse
-	) {
-		console.log("COMPLETE");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("COMPLETE");
 
-		// fetching product title from the page
-		let productName = document.querySelector("h1").innerText;
-		
-		// fetching product price from the page
-		let priceSelectors = [
-			'[data-at*="price"]',
-			'[class*="price"]',
-			'[id*="price"]',
-			'[data-test*="price"]',
-			'[data-automation*="price"]',
-		];
+  // fetching product title from the page
+  let productName = document.querySelector("h1").innerText;
 
-		let productPrice;
-		let foundPrice;
+  // fetching product price from the page
+  let priceSelectors = [
+    '[data-at*="price"]',
+    '[class*="price"]',
+    '[id*="price"]',
+    '[data-test*="price"]',
+    '[data-automation*="price"]',
+  ];
 
-		// function to find text nodes with the  $ sign
-		const dollarFinder = (node) => {
-			let child, next;
-			if (node.nodeType === 3) {
-				console.log(node)
-				if (
-					(node.nodeValue.includes("$") || node.nodeValue.includes("CAD")) &&
-					node.nodeValue.length < 40
-				) {
-					foundPrice = node.nodeValue;
-					return foundPrice;
-				}
-			} else {
-				console.log("go to childnodes")
-				child = node.firstChild;
-				while (child) {
-					next = child.nextSibling;
-					foundPrice = dollarFinder(child);
-					console.log("found price " + foundPrice)
-					if (foundPrice) {
-						productPrice = foundPrice;
-						break;
-					}
-					child = next;
-				}
-			}
-			return foundPrice
-		};
+  let productPrice;
+  let foundPrice;
 
-		// function to find DOM elements with selectors from priseSelectors
-		let priceFinder = function () {
-			for (let selector of priceSelectors) {
-				let priceElement = document.querySelector(selector);
-				if (priceElement) {
-					console.log(priceElement.innerText);
-					if (priceElement.children.length === 0) {
-						console.log("return inner text");
-						console.log(priceElement);
-						console.log(priceElement.innerText);
-						foundPrice = priceElement.innerText;
-						return foundPrice;
-					} else {
-						console.log("go and search deeper");
-						dollarFinder(priceElement);
-						console.log("dollarFinder found " + foundPrice);
-						return foundPrice;
-					}
-				}
-			}
-		}
+  // function to find text nodes with the  $ sign
+  const dollarFinder = (node) => {
+    let child, next;
+    if (node.nodeType === 3) {
+      console.log(node);
+      if (
+        (node.nodeValue.includes("$") || node.nodeValue.includes("CAD")) &&
+        node.nodeValue.length < 40
+      ) {
+        foundPrice = node.nodeValue;
+        return foundPrice;
+      }
+    } else {
+      console.log("go to childnodes");
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        foundPrice = dollarFinder(child);
+        console.log("found price " + foundPrice);
+        if (foundPrice) {
+          productPrice = foundPrice;
+          break;
+        }
+        child = next;
+      }
+    }
+    return foundPrice;
+  };
 
-		productPrice = priceFinder()
-		console.log("priceFinder found " + productPrice);
+  // function to find DOM elements with selectors from priseSelectors
+  let priceFinder = function () {
+    for (let selector of priceSelectors) {
+      let priceElement = document.querySelector(selector);
+      if (priceElement) {
+        console.log(priceElement.innerText);
+        if (priceElement.children.length === 0) {
+          console.log("return inner text");
+          console.log(priceElement);
+          console.log(priceElement.innerText);
+          foundPrice = priceElement.innerText;
+          return foundPrice;
+        } else {
+          console.log("go and search deeper");
+          dollarFinder(priceElement);
+          console.log("dollarFinder found " + foundPrice);
+          return foundPrice;
+        }
+      }
+    }
+  };
 
-		// function to convert price to number
-		function priceToNumber(str) {
-			let newStr = "";
-			for (let char of str) {
-				if (Number(char) || Number(char) === 0 || char === ".") {
-					newStr += char;
-				}
-			}
-			return Number(newStr.trim());
-		}
+  productPrice = priceFinder();
+  console.log("priceFinder found " + productPrice);
 
-		//product URL
-		let productUrl = window.location.href;
-		console.log("URL ", productUrl);
-		product.url = productUrl;
+  // function to convert price to number
+  function priceToNumber(str) {
+    let newStr = "";
+    for (let char of str) {
+      if (Number(char) || Number(char) === 0 || char === ".") {
+        newStr += char;
+      }
+    }
+    return Number(newStr.trim());
+  }
 
-		//store Name
-		let store_hostname = window.location.hostname;
-		//removes www. and .com
-		let one = store_hostname.substring(4);
-		let period = one.indexOf(".");
-		let store_name = one.substring(0, period);
-		console.log("Store Name ", store_name);
-		//to uppercase first letter and put it in object to be sent out
-		upper = store_name.charAt(0).toUpperCase();
-		rest = store_name.slice(1);
-		product.store = upper + rest;
+  //product URL
+  let productUrl = window.location.href;
+  console.log("URL ", productUrl);
+  product.url = productUrl;
 
-		const imgGetter = () => {
-    	for (let image of document.images) {
-    	  if (image.height > 500 && !image.src.includes("LOADING")) {
-    	    return image.src;
-    	  }
-    	}
-   };
-   const img = imgGetter();
-   console.log("************", img);
-   product.img = img;
-		
-		product.title = productName;
-		if (productPrice) 
-		{product.price = priceToNumber(productPrice)};
-		
-		// sending a responce to extension with a product object
-		console.log(product);
-		console.log(
-			sender.tab
-				? "from a content script:" + sender.tab.url
-				: "from the extension"
-		);
-		sendResponse({ farewell: product });
-	});
-	
+  //store Name
+  const storeName = (domain) => {
+    let first = domain.indexOf(".") + 1;
+    let last = domain.lastIndexOf(".");
+    let pre = domain.substring(first, last);
+    upper = pre.charAt(0).toUpperCase();
+    rest = pre.slice(1);
+    store = upper + rest;
+    console.log(store);
+  };
+
+  product.store = storeName(window.location.hostname);
+
+  const imgGetter = () => {
+    for (let image of document.images) {
+      if (image.height > 500 && !image.src.includes("LOADING")) {
+        return image.src;
+      }
+    }
+  };
+  const img = imgGetter();
+  console.log("************", img);
+  product.img = img;
+
+  product.title = productName;
+  if (productPrice) {
+    product.price = priceToNumber(productPrice);
+  }
+
+  // sending a responce to extension with a product object
+  console.log(product);
+  console.log(
+    sender.tab
+      ? "from a content script:" + sender.tab.url
+      : "from the extension"
+  );
+  sendResponse({ farewell: product });
+});
