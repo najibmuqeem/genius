8e417d9b31e9a22cc7d2d5b14f469c73e58fc337
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-13 18:43:34 -0400

    rr

--- src/fetchers.js
+++ src/fetchers.js
@@ -17,12 +17,13 @@ async function postData(url = "", data = {}) {
 }
 
 async function getData(url = "", data = {}) {
-  // Default options are marked with *
+  //Default options are marked with *
+  
   const response = await fetch(url, {
     //method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    credentials: "omit", // include, *same-origin, omit
     // headers: {
     //   "Content-Type": "application/json",
     //   // 'Content-Type': 'application/x-www-form-urlencoded',
