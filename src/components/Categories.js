22a80a7a17760a376fa85c08e9b8470e08a94646
NoahNovick <nnovickfaille1@gmail.com>
2020-04-13 18:24:32 -0400

    get requests not quite working

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,17 @@
-import React from "react";
-let classNames = require("classnames");
 import Category from "./Category.js";
+import { getCategoriesForUser } from "../fetchers.js";
+import React from "react";
+//let classNames = require("classnames");
 
 export default function Products() {
-	return <Category />;
+  const user = 1;
+  const categories = getCategoriesForUser(user).then((a) => {
+    console.log(a);
+  });
+  console.log(getCategoriesForUser(user));
+
+  // const categoryList = categories.map((category) => {
+  //   return <Category />;
+  // });
+  return <div>hi</div>;
 }
--- src/fetchers.js
+++ src/fetchers.js
@@ -19,47 +19,51 @@ async function postData(url = "", data = {}) {
 async function getData(url = "", data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    //method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
+    // headers: {
+    //   "Content-Type": "application/json",
+    //   // 'Content-Type': 'application/x-www-form-urlencoded',
+    // },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
 const addProduct = (product) => {
-  postData("localhost:8000/products", product);
+  postData("http://localhost:8000/products", product);
 };
 
 const addCategory = (user_id, category_name) => {
-  postData("localhost:8000/categories", { user_id, category_name });
+  postData("http://localhost:8000/categories", { user_id, category_name });
 };
 
 const addFriends = (user_1_id, user_2_id) => {
-  postData("localhost:8000/friends", { user_1_id, user_2_id });
+  postData("http://localhost:8000/friends", { user_1_id, user_2_id });
 };
 
 const addUser = (username, email, birthday, avatar) => {
-  postData("localhost:8000/users", { username, email, birthday, avatar });
+  postData("http://localhost:8000/users", {
+    username,
+    email,
+    birthday,
+    avatar,
+  });
 };
 
 const getProductsForCategory = (user_id, category_id) => {
-  getData(`localhost:8000/products/c/${category_id}`, { user_id });
+  getData(`http://localhost:8000/products/c/${category_id}`, { user_id });
 };
 
 const getCategoriesForUser = (user_id) => {
-  getData(`localhost:8000/categories/${user_id}`);
+  return getData(`http://localhost:8000/categories/${user_id}`);
 };
 
 const getProductsForUser = (user_id) => {
-  getData(`localhost:8000/products/u/${user_id}`);
+  getData(`http://localhost:8000/products/u/${user_id}`);
 };
 
 export {
