0377961b2eb1fe44b62d565c01ea87d7f029895f
NoahNovick <nnovickfaille1@gmail.com>
2020-04-14 17:53:47 -0400

    render category specific products

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,14 +4,13 @@ import { Link } from "react-router-dom";
 //let classNames = require("classnames");
 export default function Category(props) {
   return (
-    <li class="category">
-      <Link to="/products">
-        {/* <button onClick={props.getCategoryId(props.id)}>click</button> */}
-      </Link>
-      <h4>{props.name}</h4>
-      <img src="" width="180" height="180" />
-      <p> count: {props.count} </p>
-      <p> items sum: ${props.sum / 100} </p>
-    </li>
+    <Link to="/products">
+      <li class="category" onClick={() => props.getCategoryId(props.id)}>
+        <h4>{props.name}</h4>
+        <img src="" width="180" height="180" />
+        <p> count: {props.count} </p>
+        <p> items sum: ${props.sum / 100} </p>
+      </li>
+    </Link>
   );
 }
