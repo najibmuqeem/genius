81d3006981e3638b1f8dbe15c623ca1e501854b0
NoahNovick <nnovickfaille1@gmail.com>
2020-04-21 18:20:10 -0400

    removed console.logs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,19 @@ import "./components_styles/footer.css";
 
 export default function Footer() {
   return (
-  <footer>
-		<ul class="footer-list">
-			<li class="footer-list__item">
-				<a href="mailto: undefined@gmail.com">Contact us</a>
-			</li>
-			<li class="footer-list__item">XYZ</li>
-			<li class="footer-list__item">&copy; Undefined</li>
-		</ul>
-	</footer>)
-  
+    <footer>
+      <ul class="footer-list">
+        <li class="footer-list__item">
+          <a href="mailto: undefined@gmail.com">Contact us</a>
+        </li>
+        <li class="footer-list__item">
+          <img
+            width="100"
+            src="stylesheets/images/genius_main_green_blue.png"
+          ></img>
+        </li>
+        <li class="footer-list__item">&copy; Undefined</li>
+      </ul>
+    </footer>
+  );
 }
